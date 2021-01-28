f36549c989e974390e840a71a1d64497e2e174fb
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-01-29 03:09:28 +0900

    views에서 api 만들기

--- quizApp/views.py
+++ quizApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerilaizer
+import random
 
-# Create your views here.
+@api_view(['GET'])
+def randomQuizAPI(request, id):
+    '''개수(id)가 주어졌을때 그 개수만큼 랜덤한 퀴즈 객체 반환해주는 API'''
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs),id)
+    serializer = QuizSerilaizer(randomQuizs, many=True)
+    return Response(serializer.data)
